9c86588567f6474eb64801fdc6c9e2a71a97bdd3
openeyes-lab <74488113+openeyes-lab@users.noreply.github.com>
2021-02-11 11:26:09 +0100

    Update sd109.h
    copyright fix

--- build/sd109.h
+++ build/sd109.h
@@ -7,9 +7,14 @@
  *
  * Include file of sd109-hwmon Linux driver
  *
+ * This file is part of sd109-hwmon distribution
+ * https://github.com/openeyes-lab/sd109-hwmon
+ *
+ * Copyright (c) 2021 OPEN-EYES Srl 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
